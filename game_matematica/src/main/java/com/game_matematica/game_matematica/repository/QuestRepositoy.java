c836384ac26a7a247f3d73473c3be5497d0749f3
Gabriel Melo laptop <contatogabemelo@gmail.com>
2025-11-10 10:35:07 -0300

    update folders

--- game_matematica/src/main/java/com/game_matematica/game_matematica/repository/QuestRepositoy.java
+++
@@ -1,8 +0,0 @@
-package com.game_matematica.game_matematica.repository;
-
-import com.game_matematica.game_matematica.model.QuestModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestRepositoy extends JpaRepository<QuestModel,Long> {
-
-}
